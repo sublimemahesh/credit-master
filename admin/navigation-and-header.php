081ad315e7124297c4dfc10ecdf8fc29669c9c7a
chalanadulajdilshan <chalanadulaj98@gmail.com>
2018-10-12 15:23:10 +0530

    #26 Manage loan.php (Create manage_loan.php)

--- admin/navigation-and-header.php
+++ admin/navigation-and-header.php
@@ -282,7 +282,7 @@ include_once(dirname(__FILE__) . './auth.php');
                             </a>
                         </li>
                         <li>
-                            <a href="manage-users.php">
+                            <a href="manage-loan.php">
                                 <i class="material-icons">list</i>
                                 <span>Manage</span>
                             </a>
