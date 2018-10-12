a439d3d0ae615c65a395b1ebbf6f4f377ba0fefc
chalanadulajdilshan <chalanadulaj98@gmail.com>
2018-10-12 16:12:20 +0530

    #28 Edit loan details (Create Edit_loan.php)

--- admin/post-and-get/loan.php
+++ admin/post-and-get/loan.php
@@ -1,9 +1,10 @@
 <?php
+
 include_once(dirname(__FILE__) . '/../../class/include.php');
 
 
 if (isset($_POST['add-loan'])) {
-    
+
     $LOAN = new Loan(NULL);
     $VALID = new Validator();
 
@@ -22,7 +23,6 @@ if (isset($_POST['add-loan'])) {
         'customer' => ['required' => TRUE],
         'guarantor_1' => ['required' => TRUE],
         'loan_amount' => ['required' => TRUE],
-
     ]);
 
 
@@ -32,14 +32,12 @@ if (isset($_POST['add-loan'])) {
 
         if (!isset($_SESSION)) {
             session_start();
-
         }
 
         $VALID->addError("Your data was saved successfully", 'success');
         $_SESSION['ERRORS'] = $VALID->errors();
 
         header('Location: ' . $_SERVER['HTTP_REFERER']);
-
     } else {
 
         if (!isset($_SESSION)) {
@@ -49,29 +47,32 @@ if (isset($_POST['add-loan'])) {
         $_SESSION['ERRORS'] = $VALID->errors();
 
         header('Location: ' . $_SERVER['HTTP_REFERER']);
-
     }
-
 }
 
 
 
 if (isset($_POST['update'])) {
 
-    
-    $LOAN = new Center($_POST['id']);
 
-    $LOAN->center_name = $_POST['center_name'];
-    $LOAN->address =$_POST['address'];
-    $LOAN->center_leader_name = $_POST['center_leader_name'];
+    $LOAN = new Loan($_POST['id']);
+
+    $LOAN->create_date = $_POST['create_date'];
+    $LOAN->customer = $_POST['customer'];
+    $LOAN->guarantor_1 = $_POST['guarantor_1'];
+    $LOAN->guarantor_2 = $_POST['guarantor_2'];
+    $LOAN->loan_amount = $_POST['loan_amount'];
+    $LOAN->loan_period = $_POST['loan_period'];
+    $LOAN->interest_rate = $_POST['interest_rate'];
+    $LOAN->installment_type = $_POST['installment_type'];
 
 
     $VALID = new Validator();
     $VALID->check($LOAN, [
-        'center_name' => ['required' => TRUE],
-        'address' => ['required' => TRUE],
-        'center_leader_name' => ['required' => TRUE]
-
+        'create_date' => ['required' => TRUE],
+        'customer' => ['required' => TRUE],
+        'guarantor_1' => ['required' => TRUE],
+        'loan_amount' => ['required' => TRUE],
     ]);
 
 
@@ -81,7 +82,6 @@ if (isset($_POST['update'])) {
 
         if (!isset($_SESSION)) {
             session_start();
-
         }
 
         $VALID->addError("Your changes saved successfully", 'success');
@@ -89,7 +89,6 @@ if (isset($_POST['update'])) {
         $_SESSION['ERRORS'] = $VALID->errors();
 
         header('Location: ' . $_SERVER['HTTP_REFERER']);
-
     } else {
 
         if (!isset($_SESSION)) {
@@ -98,8 +97,6 @@ if (isset($_POST['update'])) {
 
         $_SESSION['ERRORS'] = $VALID->errors();
         header('Location: ' . $_SERVER['HTTP_REFERER']);
-
     }
-
 }
 
