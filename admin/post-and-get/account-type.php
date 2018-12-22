bc3b0d5d5a56fe6a7eca01267c755f8376009262
chalanadulajdilshan <chalanadulaj98@gmail.com>
2018-12-22 15:25:23 +0530

    #261 Create edit -account-type

--- admin/post-and-get/account-type.php
+++ admin/post-and-get/account-type.php
@@ -13,7 +13,6 @@ if (isset($_POST['create'])) {
 
     $VALID->check($ACCOUNT_TYPE, [
         'name' => ['required' => TRUE],
-        
     ]);
 
     if ($VALID->passed()) {
@@ -44,20 +43,16 @@ if (isset($_POST['create'])) {
 if (isset($_POST['update'])) {
 
 
-    $ACCOUNT_TYPE = new Route($_POST['id']);
+    $ACCOUNT_TYPE = new AccountType($_POST['id']);
 
     $ACCOUNT_TYPE->name = ucfirst($_POST['name']);
-    $ACCOUNT_TYPE->code = $_POST['code'];
-    $ACCOUNT_TYPE->start_location = ucfirst($_POST['start_location']);
-    $ACCOUNT_TYPE->end_location = ucfirst($_POST['end_location']);
-    $ACCOUNT_TYPE->collector = $_POST['collector'];
+
 
 
     $VALID = new Validator();
     $VALID->check($ACCOUNT_TYPE, [
         'name' => ['required' => TRUE],
-        'code' => ['required' => TRUE],
-        'collector' => ['required' => TRUE],
+        
     ]);
 
     if ($VALID->passed()) {
